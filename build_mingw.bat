b718f3d23973a15c247b90e1873dd61b59a4a86b
Knigavko, Yuri <i.knigavko@gmail.com>
2024-06-01 12:42:41 +0200

    Remove redundant line from build_mingw.bat.
    Uncomment line in main CMainList.txt.

--- build_mingw.bat
+++ build_mingw.bat
@@ -6,4 +6,3 @@ set CMAKE_PREFIX_PATH=D:\Qt\6.7.1\mingw_64
 call init_project.bat mingw "MinGW Makefiles" || exit /B 1
 cd build/mingw || exit /B 1
 mingw32-make TicTacToe -j5 || exit /B 1
-mingw32-make GameLib -j5 || exit /B 1
